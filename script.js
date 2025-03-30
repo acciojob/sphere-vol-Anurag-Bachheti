fc45ff437a9726089f19f8aeba727d4056ba3e1f
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-03-30 14:13:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-let radius = document.getElementById("radius");
-let volume = document.getElementById("volume");
-let calculate = document.getElementById("calculate");
+document.addEventListener("DOMContentLoaded", function(){
 
-function volume_sphere() {
-  let r = parseFloat(radius.value); // Convert input to number
-  let vol = (4 / 3) * Math.PI * Math.pow(r, 3); // Correct exponentiation
-  volume.value = vol.toFixed(2); // Set the value of input field with 2 decimal places
-}
+	let radius = document.getElementById("radius");
+	let volume = document.getElementById("volume");
+	let calculate = document.getElementById("calculate");
 
-calculate.addEventListener("click", volume_sphere);
+	function volume_sphere(){
+		let r = parseFloat(radius.value);
+		if(isNaN(r) || r < 0){
+			volume.value = NaN;
+		} else{
+			let vol = (4/3) * Math.PI * Math.pow(r,3);
+			volume.value = vol.toFixed(4);
+		}
+	}
+	calculate.addEventListener("click", volume_sphere)
+});
