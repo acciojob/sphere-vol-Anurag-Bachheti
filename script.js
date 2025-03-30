7724cf4f96e7b4fb12dbd55ebbcaa476faa512d5
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-03-30 14:41:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
 	let radius = document.getElementById("radius");
 	let volume = document.getElementById("volume");
-	let calculate = document.getElementById("calculate");
+	let submit = document.getElementById("submit");
 
 	function volume_sphere(){
 		let r = parseFloat(radius.value);
@@ -13,5 +13,5 @@ document.addEventListener("DOMContentLoaded", function(){
 			volume.value = vol.toFixed(4);
 		}
 	}
-	calculate.addEventListener("click", volume_sphere)
+	submit.addEventListener("click", volume_sphere)
 });
